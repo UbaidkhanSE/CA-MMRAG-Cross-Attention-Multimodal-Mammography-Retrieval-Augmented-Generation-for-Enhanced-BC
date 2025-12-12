a8fdff7fba09f410038ea2fbb42c10cc549a3be4
Ubaid Ullah <163047010+UbaidkhanSE@users.noreply.github.com>
2025-12-12 15:51:57 +0500

    Add files via upload

--- complete_pipeline.py
+++ complete_pipeline.py
@@ -46,7 +46,7 @@ UNIFIED_EMBEDDINGS_PATH = STORAGE_BASE / "embeddings" / "unified_768_embeddings.
 UNIFIED_METADATA_PATH = STORAGE_BASE / "metadata" / "unified_768_metadata.json"
 """
 
-DATASET_PATH = r"C:\mammography_gpt\Dataset\clean_complete_multimodal_dataset.csv"
+DATASET_PATH = r"Dataset\clean_complete_multimodal_dataset.csv"
 STORAGE_BASE = Path("mammography_retrieval_storage")
 CROSS_ATTENTION_MODEL_PATH = STORAGE_BASE / "models" / "cross_attention_best.pth"
 UNIFIED_EMBEDDINGS_PATH = STORAGE_BASE / "embeddings" / "unified_768_embeddings.npy"
